9bbed4570f98b958370c88ee015fa1b02677c517
Simon Wuyts <hi@simonwuyts.com>
2023-04-19 10:57:49 +0200

    Add stats tracking

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import ViteRadar from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteRadar({
+      enableDev: true,
+      analytics: {
+        id: 'G-5PN6PGV6ZC',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
